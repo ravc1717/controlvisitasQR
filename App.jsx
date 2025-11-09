3757a1cdac7021d9f903e71e1be34aa9aa619a63
Rodrigo <ravc1717@gmail.com>
2025-11-09 09:33:42 -0600

    Primera versión del sistema de control de visitas

---
+++ App.jsx
@@ -0,0 +1,13 @@
+
+import TestSupabase from "./TestSupabase";
+
+function App() {
+  return (
+    <div>
+      <h1>Control de Visitas QR</h1>
+      <TestSupabase />
+    </div>
+  );
+}
+
+export default App;
